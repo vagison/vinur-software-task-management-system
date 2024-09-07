aa8bd92ccc29bdc37ee40b0d12dc749d14400bae
vagison <vagison@gmail.com>
2024-09-07 21:23:06 +0400

    Removed unused constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,7 @@
 import errorMessagesConstants from './messages/error';
 import responseMessagesConstants from './messages/response';
-import warningMessagesConstants from './messages/warning';
 
 export {
   errorMessagesConstants,
   responseMessagesConstants,
-  warningMessagesConstants,
 };
